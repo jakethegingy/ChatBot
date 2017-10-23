9a7e4280cab9caf2329730c2f1be3390d4a1fceb
jakethegingy <31513857+jakethegingy@users.noreply.github.com>
2017-10-23 09:51:46 -0600

    Added movie and chatbot file.

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,5 +1,19 @@
 package chat.controller;
 
-public class ChatController {
+import chat.model.ChatView;
 
+public class ChatController 
+{
+	private ChatView chatbot;
+	
+	public ChatController()
+	{
+		chatbot = new ChatView();
+	}
+	public void start()
+	{
+		//put stuff here.
+		//Test
+	}
 }
+
---
+++ src/chat/view/PopupDisplay.java
@@ -0,0 +1,28 @@
+package chat.view;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+public class PopupDisplay 
+{
+	private ImageIcon icon;
+	private String windowTitle;
+	
+	public PopupDisplay()
+	{
+		icon = new ImageIcon(getClass().getResource("Chicken4U.png"));
+		windowTitle = "Supreme Ogre Lard Jathanual the Chatbot exclaims: ";	
+	}
+	
+	public void displayText(String message)
+	{
+		JOptionPane.showMessageDialog(null, message, windowTitle, JOptionPane.INFORMATION_MESSAGE, icon);
+	}
+	
+	public String collectResponse(String question)
+	{
+		String answer = "He is sleepy now so no meme will be granted.";
+		
+		answer += JOptionPane.showInputDialog(null, question, windowTitle, JOptionPane.PLAIN_MESSAGE, icon, null,"");
+
+		return answer;
+	}
+}
